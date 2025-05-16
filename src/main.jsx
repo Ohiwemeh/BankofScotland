7cba253826bc95c6e1b6390a64910c1e230143af
Joseph Jegede <ohiwemeh@gmail.com>
2025-05-17 00:46:15 +0100

    Update: fixed login error and cleaned up App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // ✅ Needed for routing
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
 )
