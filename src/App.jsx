e06d204462ea648a44f6c631771eeec479646d09
Joseph Jegede <ohiwemeh@gmail.com>
2025-05-17 00:50:28 +0100

    Update: fixed login error and cleaned up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
@@ -9,58 +9,47 @@ import SignUpPage from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import TransactionHistory from './pages/TransactionHistory';
 import PaymentTransfer from './pages/PaymentTransfer';
-// import MyBanks from './pages/MyBanks';
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          {/* Redirect root to /dashboard or /sign-in depending on auth */}
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+      <Routes>
+        {/* Redirect root to /dashboard or /sign-in depending on auth */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
 
-          {/* Public routes */}
-          <Route path="/sign-in" element={<LoginPage />} />
-          <Route path="/sign-up" element={<SignUpPage />} />
+        {/* Public routes */}
+        <Route path="/sign-in" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
 
-          {/* Private routes */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/transaction-history"
-            element={
-              <PrivateRoute>
-                <TransactionHistory />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/payment-transfer"
-            element={
-              <PrivateRoute>
-                <PaymentTransfer />
-              </PrivateRoute>
-            }
-          />
-          {/* <Route
-            path="/my-banks"
-            element={
-              <PrivateRoute>
-                <MyBanks />
-              </PrivateRoute>
-            }
-          /> */}
+        {/* Private routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/transaction-history"
+          element={
+            <PrivateRoute>
+              <TransactionHistory />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/payment-transfer"
+          element={
+            <PrivateRoute>
+              <PaymentTransfer />
+            </PrivateRoute>
+          }
+        />
 
-          {/* Catch-all for 404 */}
-          <Route path="*" element={<h1 className="p-8 text-center">404 – Page Not Found</h1>} />
-        </Routes>
-      </Router>
+        {/* Catch-all for 404 */}
+        <Route path="*" element={<h1 className="p-8 text-center">404 – Page Not Found</h1>} />
+      </Routes>
     </AuthProvider>
   );
 }
